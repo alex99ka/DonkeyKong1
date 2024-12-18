1913180b8b851f55f26829a79493d9a75b2102cd
alex99ka <alex99ka@gmail.com>
2024-12-18 16:14:54 +0200

    heyds

--- Board.cpp
+++ Board.cpp
@@ -194,7 +194,7 @@ int CBoard::CreateListOfLadders(ListOfLadders* lst, int size)
 {
 	int counter = 0;
 	Ladder* node;
-	int IndexOfLaddersArr[] = { 10,50,43,13,53,30,20,50,63 };
+	int IndexOfLaddersArr[] = { 10,50,43,13,53,30,20,50,63 }; //
 	int AmountOfFLadders[] = { 2,1,2,1,1,1,1 };
 	int SizeOfLadderForLevel[] = { 2,3,4,12,5,3,2 };
 	for (int i = 0; i < size; i++)
